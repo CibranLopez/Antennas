36bbca369300fc19c992dce7192e3234dbe7bd7a
CibranLopez <49532916+CibranLopez@users.noreply.github.com>
2019-04-25 22:31:06 +0200

    Update Anthens.py

--- Anthens.py
+++ Anthens.py
@@ -8,7 +8,7 @@ import time as time
 
 t0=time.clock()
 #Parámetros
-s=float(-25)
+s=float(-15)
 n=6
 
 
@@ -79,7 +79,7 @@ def G2(m,n): #Función F
     else:
         h1=1
         for i in np.arange(1,n):
-            h1*=(1-((zeros[m]**2*(A**2+(n-0.5)**2))/(zeros[n]**2*(A**2+(i-0.5)**2))))
+            h1*=(1-(zeros[m]/u[i])**2)
         h2=1
         for i in np.arange(1,n):
             if m!=i:
@@ -104,34 +104,29 @@ pl.ylim(0,1)
 #pl.savefig('Anthens3.png',dpi=300)'''
 
 
-matrix=np.loadtxt('datos.txt')
-N=matrix[0]; I2=matrix[1]
+#Páxina 254 ---------------------------------------------------------------------
+
+N=np.loadtxt('datos.txt')[0]
 
 
-lamb=2.12 #npi
-a=5*lamb; d=a/10 #Distancia uniforme entre círculos
-kt=2*np.pi/lamb
-
-
-M=len(N); ka=int(max(N)) #Para meter de novo simetría facemos ka=int(max(N)/4). Ollo liña 147.
-rho=np.zeros([M,ka]); beta=np.zeros([M,ka])
+d=0.5 #Distancia uniforme entre círculos
+M=len(N); ka=int(max(N)/4)
+rho=np.zeros(M); beta=np.zeros([M,ka])
         
 
-for i in range(M): #Páxina 249
+for i in range(M):
     for j in range(ka):
-        rho[i,j]=d*np.sqrt((i+0.5)**2+(j+0.5)**2) #Podo eliminar unha dimensión
+        rho[i]=d*(i+0.5)
         beta[i,j]=(2*j+1)*np.pi/N[i]
 
-
-I=g0(rho*np.pi/a,n)
+I=g0(rho*np.pi/5,n)
 
 def F(theta,phi,n): #Páxina 253
     aux=0
     for m in range(M):
-        for n in range(int(N[m])):                
-            aux+=I[m,0] * np.cos(kt*rho[m,0]*np.cos(beta[m,n])*np.sin(theta)*np.cos(phi)) * np.cos(kt*rho[m,0]*np.sin(beta[m,n])*np.sin(theta)*np.sin(phi))       
-    return aux
-
+        for n in range(int(N[m])/4):                
+            aux+=I[m] * np.cos(2*np.pi*rho[m]*np.cos(beta[m,n])*np.sin(theta)*np.cos(phi)) * np.cos(2*np.pi*rho[m]*np.sin(beta[m,n])*np.sin(theta)*np.sin(phi))       
+    return 4*aux
 
 F1=np.abs(F(c,0,n))
 F1=F1/max(F1)
@@ -141,9 +136,9 @@ for i in range(len(F1)):
 
 #Esta interesa 
 '''
-pl.figure(5) #Páxina 254
+pl.figure(4) #Páxina 254
 pl.plot(c,F1,label=u'-')
-pl.xlabel('$\Theta$ (rad)')
+pl.xlabel('Páxina 254')
 pl.legend(loc='upper right')
 pl.xlim(0,np.pi/2)
 pl.ylim(-50,0)
@@ -164,7 +159,7 @@ caso2=np.abs(G1(c,n)); caso2=caso2/max(caso2)
 #Caso 3 ---------------------------------------------------------------------
     
 u=[0, 0.5967, 1.7837, 3.6420, 4.3039, 5.2129]
-v=[0,0.5225, 0.5268, 0, 0, 0]
+v=[0, 0.5225, 0.5268, 0, 0, 0]
 
 def Fim(x,n): #Traballando con imaxinarios
     h1=1
@@ -177,14 +172,37 @@ def Fim(x,n): #Traballando con imaxinarios
 
 caso3=np.abs(Fim(c,n)); caso3=caso3/max(caso3)
 
+def Lopital(m,n):
+    if m==0:
+        return 1
+    else:
+        h1=1
+        for j in np.arange(1,n):
+            h1*=(np.sqrt(u[j]**4+v[j]**4+zeros[m]**4+2*u[j]**2*v[j]**2+2*zeros[m]**2*(v[j]**2-u[j]**2))/(u[j]**2+v[j]**2))
+        h2=1
+        for j in np.arange(1,n):
+            if m!=j:
+                h2*=(1-(zeros[m]/zeros[j])**2)
+        return -f0(np.pi*zeros[m])*h1/h2
+
+def gim(x,n):
+    h=0
+    for i in range(n):
+        h+=((Lopital(i,n)*f0(x*zeros[i]))/(f0(np.pi*zeros[i])**2))
+    return 4*h/(np.pi**2)
+
+g=gim(c,n)
+g=g/max(g)
+
+
 for i in range(len(caso3)):
     caso1[i]=20*math.log10(caso1[i])
     caso2[i]=20*math.log10(caso2[i])
     caso3[i]=10*math.log10(caso3[i])
 
-#Estas tres interesan
+#Estas catro interesan
 '''
-pl.figure(6) #Mesmo do principio, modificando diferente a posición dos raíces
+pl.figure(5) #Mesmo do principio, modificando diferente a posición dos raíces
 pl.plot(c,caso1,label=u'n=%i'%n)
 pl.legend(loc='upper right')
 pl.ylim(-50,0)
@@ -192,7 +210,7 @@ pl.xlim(0,20)
 pl.xlabel('Primeiro caso')
 pl.savefig('Caso1.png',dpi=300)
 
-pl.figure(7) #Segundo caso
+pl.figure(6) #Segundo caso
 pl.plot(c,caso2,label=u'n=%i'%n)
 pl.legend(loc='upper right')
 pl.ylim(-50,0)
@@ -208,36 +226,42 @@ pl.xlim(0,10)
 pl.xlabel('Terceiro caso')
 #pl.savefig('Caso3.png',dpi=300)'''
 
-'''
-x=np.arange(-np.pi/2,np.pi/2,0.01); x=np.delete(x,np.where(x==0))
-y=np.arange(-np.pi/2,np.pi/2,0.01); y=np.delete(x,np.where(x==0))
+pl.figure(8)
+pl.plot(c,g,label=u'Amplitude')
+#pl.plot(c,g,label=u'Fase')
+pl.legend(loc='upper right')
+pl.xlim(0,np.pi)
+#pl.ylim(0,1)
+#pl.savefig('Anthens3.png',dpi=300)'''
+
+
+x=np.arange(-10,10,0.1); x=np.delete(x,np.where(x==0))
+y=np.arange(-10,10,0.1); y=np.delete(x,np.where(x==0))
 X,Y=np.meshgrid(x,y)
         
 phi=np.abs(np.arctan(X/Y))
 r=np.sqrt(X**2+Y**2)
 
-z=np.abs(F(r,phi,n)) #Libro
-
-nf,nc=np.shape(z)
-Z=np.zeros([nf,nc])
+nf,nc=np.shape(Z)
+Z=np.abs(Fim(r,n)) #Libro
+Z=Z/np.max(Z)
 
 for i in range(nf):
     for j in range(nc):
-        Z[i,j]=20*math.log10(z[i,j])
-
-Z=Z-np.max(Z)
+        Z[i,j]=10*math.log10(Z[i,j])
 
 #Representación 3D
-
-fig = pl.figure()
+        
+fig = pl.figure(10)
 ax = pl.axes(projection='3d')
-ax.contour3D(X, Y, Z, 100, cmap='viridis')
-ax.set_xlabel('x')
-ax.set_ylabel('y')
-ax.set_zlabel('z')'''
+ax.plot_surface(X, Y, Z, cmap='viridis')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
 
+#ax.set_zlim(-50,0)
 #ax.view_init(0, 0)
-#pl.savefig('3D.png',dpi=500)
+#pl.savefig('3D.png',dpi=500)'''
 
 
 
