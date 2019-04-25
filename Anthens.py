eb84fa8772bd382188bc0713a02bd58ea8ef5b21
CibranLopez <49532916+CibranLopez@users.noreply.github.com>
2019-04-25 19:30:13 +0200

    Update Anthens.py

--- Anthens.py
+++ Anthens.py
@@ -5,11 +5,10 @@ import scipy as sc
 import math as math
 from mpl_toolkits.mplot3d import Axes3D
 import time as time
-#from matplotlib.ticker import LinearLocator, FormatStrFormatter
 
 t0=time.clock()
 #Parámetros
-s=float(-15)
+s=float(-25)
 n=6
 
 
@@ -116,18 +115,6 @@ kt=2*np.pi/lamb
 
 M=len(N); ka=int(max(N)) #Para meter de novo simetría facemos ka=int(max(N)/4). Ollo liña 147.
 rho=np.zeros([M,ka]); beta=np.zeros([M,ka])
-
-
-#Parametrización da elipse
-'''d1=1; D1=1
-for i in range(M):
-    for j in range(ka):
-        if i!=0:
-            phi=np.arctan(float(j)/i)
-        else:
-            phi=np.pi/2
-        rho[i,j]=(2*j+1)*d1*D1/np.sqrt((d1*np.sin(phi))**2+(D1*np.cos(phi))**2)
-        beta[i,j]=(2*j+1)*np.pi/N[i]'''
         
 
 for i in range(M): #Páxina 249
@@ -135,23 +122,6 @@ for i in range(M): #Páxina 249
         rho[i,j]=d*np.sqrt((i+0.5)**2+(j+0.5)**2) #Podo eliminar unha dimensión
         beta[i,j]=(2*j+1)*np.pi/N[i]
 
-#-----
-#'''
-for i in range(M):
-    for j in range(ka):
-        beta[i,j]=(2*j+1)*np.pi/N[i]
-
-c1=np.arange(-M/2,M/2,0.1)
-c2=np.arange(-M/2,M/2,0.1) 
-X,Y=np.meshgrid(c1,c2)
-r=np.round(np.sqrt(X**2+Y**2),0) 
-
-nun=np.zeros(M/2)
-for i in range(M/2): #nun=[  1.   8.  12.  16.  32.]
-    nun[i]=np.count_nonzero(r==i)
-I=g0(r*np.pi/a,n)
-
-
 
 I=g0(rho*np.pi/a,n)
 
@@ -162,18 +132,6 @@ def F(theta,phi,n): #Páxina 253
             aux+=I[m,0] * np.cos(kt*rho[m,0]*np.cos(beta[m,n])*np.sin(theta)*np.cos(phi)) * np.cos(kt*rho[m,0]*np.sin(beta[m,n])*np.sin(theta)*np.sin(phi))       
     return aux
 
-'''
-#-----
-
-I=g0(rho*np.pi/a,n)
-
-def F(theta,phi,n): #Páxina 253
-    aux=0
-    for m in range(M):
-        for n in range(int(N[m])):                
-            aux+=I[m,0] * np.cos(kt*rho[m,0]*np.cos(beta[m,n])*np.sin(theta)*np.cos(phi)) * np.cos(kt*rho[m,0]*np.sin(beta[m,n])*np.sin(theta)*np.sin(phi))       
-    return aux
-'''
 
 F1=np.abs(F(c,0,n))
 F1=F1/max(F1)
@@ -182,33 +140,52 @@ for i in range(len(F1)):
     F1[i]=20*math.log10(F1[i])
 
 #Esta interesa 
-
+'''
 pl.figure(5) #Páxina 254
 pl.plot(c,F1,label=u'-')
 pl.xlabel('$\Theta$ (rad)')
 pl.legend(loc='upper right')
 pl.xlim(0,np.pi/2)
 pl.ylim(-50,0)
-#pl.savefig('---.png',dpi=300)
+#pl.savefig('---.png',dpi=300)'''
 
+#Caso 1 ---------------------------------------------------------------------
 
 u=[0,1.4839, 1.8933, 2.9268, 3.9622, 5.0416]
-F2=np.abs(G1(c,n)); F2=F2/max(F2)
-
-for i in range(len(F2)):
-    F2[i]=20*math.log10(F2[i])
+caso1=np.abs(G1(c,n)); caso1=caso1/max(caso1)
 
 
+#Caso 2 ---------------------------------------------------------------------
+    
 u=[0,0.6322, 1.9308, 3.7674, 4.3929, 5.2633]
-F3=np.abs(G1(c,n));F3=F3/max(F3)
+caso2=np.abs(G1(c,n)); caso2=caso2/max(caso2)
 
-for i in range(len(F3)):
-    F3[i]=20*math.log10(F3[i])
 
-#Estas dúas interesan
+#Caso 3 ---------------------------------------------------------------------
+    
+u=[0, 0.5967, 1.7837, 3.6420, 4.3039, 5.2129]
+v=[0,0.5225, 0.5268, 0, 0, 0]
+
+def Fim(x,n): #Traballando con imaxinarios
+    h1=1
+    for j in np.arange(1,n):
+        h1*=((u[j]**4+v[j]**4+x**4+2*u[j]**2*v[j]**2+2*x**2*(v[j]**2-u[j]**2))/((u[j]**2+v[j]**2)**2))
+    h2=1
+    for j in np.arange(1,n):
+        h2*=1-(x/zeros[j])**2
+    return (f(x)/(h2*np.pi*x))**2*h1
+
+caso3=np.abs(Fim(c,n)); caso3=caso3/max(caso3)
+
+for i in range(len(caso3)):
+    caso1[i]=20*math.log10(caso1[i])
+    caso2[i]=20*math.log10(caso2[i])
+    caso3[i]=10*math.log10(caso3[i])
+
+#Estas tres interesan
 '''
 pl.figure(6) #Mesmo do principio, modificando diferente a posición dos raíces
-pl.plot(c,F2,label=u'n=%i'%n)
+pl.plot(c,caso1,label=u'n=%i'%n)
 pl.legend(loc='upper right')
 pl.ylim(-50,0)
 pl.xlim(0,20)
@@ -216,15 +193,22 @@ pl.xlabel('Primeiro caso')
 pl.savefig('Caso1.png',dpi=300)
 
 pl.figure(7) #Segundo caso
-pl.plot(c,F3,label=u'n=%i'%n)
+pl.plot(c,caso2,label=u'n=%i'%n)
 pl.legend(loc='upper right')
 pl.ylim(-50,0)
 pl.xlim(0,20)
 pl.xlabel('Segundo caso')
 pl.savefig('Caso2.png',dpi=300)
+
+pl.figure(7) #Terceiro caso
+pl.plot(c,caso3,label=u'n=%i'%n)
+pl.legend(loc='upper right')
+pl.ylim(-50,0)
+pl.xlim(0,10)
+pl.xlabel('Terceiro caso')
+#pl.savefig('Caso3.png',dpi=300)'''
+
 '''
-
-
 x=np.arange(-np.pi/2,np.pi/2,0.01); x=np.delete(x,np.where(x==0))
 y=np.arange(-np.pi/2,np.pi/2,0.01); y=np.delete(x,np.where(x==0))
 X,Y=np.meshgrid(x,y)
@@ -244,7 +228,7 @@ for i in range(nf):
 Z=Z-np.max(Z)
 
 #Representación 3D
-'''
+
 fig = pl.figure()
 ax = pl.axes(projection='3d')
 ax.contour3D(X, Y, Z, 100, cmap='viridis')
